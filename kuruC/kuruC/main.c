5215af9259eabf41953dc680031546c8335b005a
mice73jp <mice73jp@yahoo.co.jp>
2016-11-09 00:23:40 +0900

    C言語のトレイニング：初回コミット

---
+++ kuruC/kuruC/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  kuruC
+//
+//  Created by 蔡 ミンソク on 2016. 10. 31..
+//  Copyright © 2016년 蔡 ミンソク. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("Hello");
+    printf("World\n");
+    printf("\n\n");
+    printf("Windows 2000\tMicrosoft\n");
+    printf("Mac OS X\tApple\n");
+    return 0;
+}
